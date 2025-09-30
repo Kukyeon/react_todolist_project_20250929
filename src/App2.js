d8952d9b6c3ce0de096c18020dd743022e9a3752
Kukyeon <rnrdus2000@naver.com>
2025-09-30 11:31:48 +0900

    useReducer 업데이트 함수

--- src/App2.js
+++ src/App2.js
@@ -3,9 +3,19 @@ import './App.css';
 import Header from './component/Header';
 import TodoEditor from './component/TodoEditor';
 import TodoList from './component/TodoList';
-import { useRef, useState  } from 'react';
+import { useRef, useState , useReducer } from 'react';
 
-
+function reducer(state, action){
+  
+  switch (action.type) {
+    case "CREATE":
+      {return [action.newItem, ...state]}
+      
+  
+    default:
+      return state;
+  }
+};
 
 function App() {
 
@@ -30,41 +40,31 @@ function App() {
     }
   ];
 
-  const [todo, setTodo] = useState(mockTodo);
+ // const [todo, setTodo] = useState(mockTodo);
+  const [todo, dispatch] = useReducer(reducer, mockTodo);
 
   const idRef = useRef(3); // 초기값이 3인 ref 객체 생성하여 idRef에 저장
 
   function onCreate(content){
-    const newItem = {
+    
+    dispatch(
+      {type:"CREATE" , newItem:{
       id : idRef.current, // idRef가 현재 저장하고 있는 값을 불러옴
       content, 
       isDone : false ,
       createDate : new Date().getTime(),
-    };
-    setTodo([newItem, ...todo]);
-    //newItem -> 할일 객체를 생성한 후 idRef값을 1증가
+    }}
+    );
     idRef.current += 1;
 
-  }
+  };
 
   function onUpdate(targetId){
-    setTodo(
-    todo.map((it) =>{
-      
-      if(it.id === targetId){ 
-  // 할일 아이템을 반복하다가 들어온 targetId와 현재 읽고있는 글의 아이템의 id와 일치하면 참
-        return{
-          ...it, isDone: !it.isDone
-        };
-      }else{
-        return it;
-      }
-    }));
+   
   }
 
   function onDelete(targetId){
-    setTodo(todo.filter((it) => it.id !== targetId))
-    //삭제를 클릭한 id 아이템을 제외한 나머지 할일 리스트를 투두에 저장
+    
   }
 
   return (
--- src/component/TestComp.js
+++ src/component/TestComp.js
@@ -10,7 +10,9 @@ function reducer(state, action) { // 상태 변화 함수
             return state + action.data;
         case "DECREASE":
             return state - action.data;    
-    
+        case "INIT":
+            state = action.data;
+            return state;
         default:
            return state;
     }
@@ -29,6 +31,7 @@ function TestComp(){
             <div>
                 <button onClick={()=>dispatch({type:"INCREASE", data:1})}>증가</button>
                 <button onClick={()=>dispatch({type:"DECREASE", data:2})}>감소</button>
+                 <button onClick={()=>dispatch({type:"INIT", data:0})}>초기화</button>
             </div>
         </div>
     );
